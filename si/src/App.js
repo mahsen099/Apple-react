fef2e9c1c30a17317912cf0409306bcf53fdd8e0
mahsen099 <75514973+mahsen099@users.noreply.github.com>
2021-02-08 03:28:38 -0600

    Initial commit

---
+++ si/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Footer from './Component/Footer/Footer';
+import Nav from './Component/Nav/Nav';
+import Alertsection from './Component/Alertsection/Alertsection';
+import Main1 from './Component/Main1/Main1';
+import Main2 from './Component/Main2/Main2';
+import Main3 from './Component/Main3/Main3';
+import Main4 from './Component/Main4/Main4';
+import Main5 from './Component/Main5/Main5';
+import Main6 from './Component/Main6/Main6';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Alertsection />
+      <Main1 />
+      <Main2 />
+      <Main3 />
+      <Main4 />
+      <Main5 />
+      <Main6 />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ si/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/styles.css';
+import './css/bootstrap.css';
+import App from './App';
+import fontss from './index.css';
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
+
+
+
